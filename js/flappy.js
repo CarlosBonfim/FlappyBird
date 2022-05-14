d9bdcc9fb60e1e109b27d8ba2139311380c39419
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2022-05-14 17:56:12 -0400

    alteracao no voo do passaro

--- js/flappy.js
+++ js/flappy.js
@@ -89,7 +89,7 @@ function Passaro(alturaJogo) {
     this.setY(alturaJogo / 2)
 
     this.animar = () => {
-        const novoY = this.getY() + (voando ? 2 : -5)
+        const novoY = this.getY() + (voando ? 3 : -4)
         const alturaMaxima = alturaJogo - this.elemento.clientHeight
 
         if (novoY <= 0) {

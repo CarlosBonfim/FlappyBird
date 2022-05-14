25dbd43a7a6c32de00dd2d3046db32ead7eeab23
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2022-05-14 17:02:15 -0400

    correção

--- js/flappy.js
+++ js/flappy.js
@@ -56,7 +56,7 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
         new ParDeBarreiras(altura, abertura, largura + espaco * 3)
     ]
 
-    const deslocamento = 3
+    const deslocamento = 5
     this.animar = () => {
         this.pares.forEach(par => {
             par.setX(par.getX() - deslocamento)

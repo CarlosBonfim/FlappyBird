f560c8569c208e17c9d79ddc38f5d1c3a98a0844
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2022-05-14 20:06:59 -0400

    alteração na velocidade do deslocamento

--- js/flappy.js
+++ js/flappy.js
@@ -56,7 +56,7 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
         new ParDeBarreiras(altura, abertura, largura + espaco * 3)
     ]
 
-    const deslocamento = 1
+    const deslocamento = 2
     this.animar = () => {
         this.pares.forEach(par => {
             par.setX(par.getX() - deslocamento)
